787701e855666f855e75b6965e28b5aab8fbe268
hyzoom <hazem.abdalazeem@gmail.com>
2015-11-21 23:05:58 +0200

    Hazem 20151121 : add Estate Details Fragment

---
+++ realestate/src/main/java/com/trioplus/realestate/fragment/EstateDetailsFragment.java
@@ -0,0 +1,12 @@
+package com.trioplus.realestate.fragment;
+
+/**
+ * Created by hp on 21/11/2015.
+ */
+public class EstateDetailsFragment extends CommonFragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
