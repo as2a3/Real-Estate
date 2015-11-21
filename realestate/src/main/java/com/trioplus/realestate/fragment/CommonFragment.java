625c8590700fd6659f8a4d50f9c6271794b5520d
as2a3 <ahmedelbatawy92@gmail.com>
2015-11-21 23:02:32 +0200

    Saeed 20151121 : Add HomeFragment and CommonFragment

---
+++ realestate/src/main/java/com/trioplus/realestate/fragment/CommonFragment.java
@@ -0,0 +1,15 @@
+package com.trioplus.realestate.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Ahmed on 11/21/2015.
+ */
+public class CommonFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ realestate/src/main/java/com/trioplus/realestate/fragment/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.trioplus.realestate.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.trioplus.realestate.fragment.CommonFragment;
+
+/**
+ * Created by Ahmed on 11/21/2015.
+ */
+public class HomeFragment extends CommonFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
